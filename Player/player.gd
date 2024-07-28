5905ef34ef141b56c9c321b583718fec6ca4964c
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-07-28 17:47:52 -0300

    70. Improved Aiming and Smoothing

--- Player/player.gd
+++ Player/player.gd
@@ -8,6 +8,7 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var mouse_motion := Vector2.ZERO
 
+@onready var camera_pivot: Node3D = $CameraPivot
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -46,4 +47,8 @@ func _input(event: InputEvent) -> void:
 
 func handle_camera_rotation() -> void :
 	rotate_y(mouse_motion.x)
+	camera_pivot.rotate_x(mouse_motion.y)
+	camera_pivot.rotation_degrees.x = clamp(
+		camera_pivot.rotation_degrees.x, -90.0, 90.0
+	)
 	mouse_motion = Vector2.ZERO		
