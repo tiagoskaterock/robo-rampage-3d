607d26340789fef870410acadb8dc72b31b351a6
Tiago Bordin <tiagolemespalhano@gmail.com>
2024-08-19 18:38:26 -0300

    Sons na tela de restart/quit

--- Player/game_over_menu.gd
+++ Player/game_over_menu.gd
@@ -14,9 +14,22 @@ func game_over():
 
 
 func _on_restart_button_pressed():
+	$YeahAudio.play()	
+	$TimerToRestart.start()
+
+
+func _on_quit_button_pressed():
+	$NoAudio.play()
+	
+
+func _on_timer_to_restart_timeout():
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 
-func _on_quit_button_pressed():
+func _on_timer_to_quit_timeout():
 	get_tree().quit()
+
+
+func _on_no_audio_finished():
+	$TimerToQuit.start()
